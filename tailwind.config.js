d923d9efec27f92b7bb3c934fdadfc6b1aa6cbca
kirtan0101 <kjpatel200022@gmail.com>
2024-10-14 10:18:13 +0530

    make responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'max-568': {'max': '568px'},
+      }
+    },
   },
   plugins: [],
 }
